97ab977e0af44813b3cf9c47fcc9f20ce05a9855
dylanilyas <115644950+dylanilyas@users.noreply.github.com>
2022-12-14 14:14:07 +0700

    Add files via upload

---
+++ dbcon.php
@@ -0,0 +1,6 @@
+<?php
+$host = "localhost";
+$tableprefix = "sewamobil_"; //You can change this table prefix, don't use white spaces, use underscores instead for example this_is_my_table_prefix_
+$databasename = "sewamobil";
+$dbuser = "root";
+$dbpassword = "";
